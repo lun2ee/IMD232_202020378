583ec43b12932bbc74f288afb09eb8c518c7596b
lun2ee <128441642+lun2ee@users.noreply.github.com>
2023-09-18 10:05:39 +0900

    주방을 그려봄

--- src/w2/assignment/basic/script/basicDrawing.js
+++ src/w2/assignment/basic/script/basicDrawing.js
@@ -13,41 +13,52 @@ function draw() {
   stroke(0);
   strokeWeight(1);
 
-  ellipse(100, 100, 50);
-  rect(100, 100, 50, 50);
-  ellipse(200, 100, 50, 25);
-  rect(200, 100, 25, 50);
+  rect(0, 50, 400, 500);
+  rect(0, 60, 450, 10);
+  rect(300, 55, 50, 550);
+  rect(350, 55, 50, 550);
+  rect(370, 55, 50, 550);
+  fill('yellow');
+  ellipse(500, 200, 100);
+  fill('skyblue');
+  rect(450, 120, 100, 90);
+  // ellipse(200, 100, 50, 25);
+  // rect(250, 150, 70, 80);
 
   rectMode(CENTER);
-  rect(300, 100, 50, 50);
-  ellipse(300, 100, 50, 50);
-  rect(400, 100, 50, 25);
-  ellipse(400, 100, 25, 50);
-
+  // rect(300, 100, 50, 50);
+  // ellipse(300, 100, 50, 50);
+  // rect(400, 100, 50, 25);
+  // ellipse(400, 100, 25, 50);
   fill(225, 127, 0);
-  ellipse(100, 200, 50);
-  fill('#00EE40');
-  circle(200, 200, 50);
-  colorMode(HSL);
-  fill(240, 100, 50);
-  rect(300, 200, 50);
-  noStroke();
-  square(400, 200, 50);
+  rect(300, 550, 50, 200);
+  rect(700, 550, 50, 200);
+  rect(500, 450, 500, 50);
 
-  rect(100, 300, 50, 50, 20);
-  rect(200, 300, 50, 50, 5, 10, 15, 20);
+  // fill(225, 127, 0);
+  // ellipse(100, 200, 50);
+  // fill('#00EE40');
+  // circle(200, 200, 50);
+  // colorMode(HSL);
+  // fill(240, 100, 50);
+  // rect(300, 200, 50);
+  // noStroke();
+  // square(400, 200, 50);
+
+  // rect(100, 300, 50, 50, 20);
+  // rect(200, 300, 50, 50, 5, 10, 15, 20);
 
   stroke(0);
-  line(100, 400, 150, 450);
+  // line(100, 400, 150, 450);
 
-  stroke('royalblue');
-  line(200, 400, 250, 400);
-  stroke('salmon');
-  strokeWeight(5);
-  line(250, 400, 250, 450);
-  stroke('slateBlue');
-  strokeWeight(10);
-  line(250, 450, 200, 450);
+  // stroke('royalblue');
+  // line(200, 400, 250, 400);
+  stroke('black');
+  strokeWeight(1);
+  line(500, 0, 500, 120);
+  // stroke('slateBlue');
+  // strokeWeight(10);
+  // line(250, 450, 200, 450);
 
   stroke(50, 100, 10);
   strokeWeight(2);
