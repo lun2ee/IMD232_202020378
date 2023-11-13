80a12917637bb9922e44613edd3a1a2dbe17d3a3
lun2ee <128441642+lun2ee@users.noreply.github.com>
2023-11-13 16:03:19 +0900

    1113

---
+++ src/w10/atClass/step1/script/sketch.js
@@ -0,0 +1,32 @@
+const tiles = [];
+const rowNum = 10,
+  colNum = 10;
+
+function setup() {
+  setCanvasContainer('mySketchGoesHere', 1, 1, true);
+
+  const w = width / colNum;
+  const h = w;
+  for (let row = 0; row < rowNum; row++) {
+    for (let col = 0; col < colNum; col++) {
+      const x = w * col;
+      const y = h * row;
+      const newTile = new ToggleTile(x, y, w, h);
+      tiles.push(newTile);
+    }
+  }
+  background(255);
+}
+
+function draw() {
+  background(255);
+
+  tiles.forEach((each) => {
+    each.display(mouseX, mouseY);
+  });
+}
+
+function mouseClicked() {
+  for (let idx = 0; idx < tiles.length; idx++)
+    if (tiles[idx].toggleState(mouseX, mouseY)) break;
+}
