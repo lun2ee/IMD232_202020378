7b620171eb2a394440af3e9590b68f2888b5fbaa
lun2ee <128441642+lun2ee@users.noreply.github.com>
2023-12-14 16:25:04 +0900

    bb

---
+++ src/final/finExample/script/sketch.js
@@ -0,0 +1,28 @@
+const unis = [];
+
+function setup() {
+  createCanvas('mySketchGoesHere', 1, 1, true);
+
+  for (let azimNum = 0; azimNum < 8; azimNum++) {
+    for (let zeniNum = 0; zeniNum < 8; zeniNum++) {
+      unis.push(new Uni((TAU / 16) * azimNum, (TAU / 8) * zeniNum, 300, 100));
+    }
+  }
+
+  background('black');
+}
+
+function draw() {
+  background('black');
+  drawingContext.shadowBlur = 32;
+  drawingContext.shadowColor = color(207, 7, 99);
+  translate(width / 2, height / 2);
+  stroke('white');
+  strokeWeight(1);
+  unis.forEach((eachUni) => {
+    eachUni.randomAcc();
+    eachUni.update();
+    eachUni.display();
+  });
+  console.log(unis[0]);
+}
